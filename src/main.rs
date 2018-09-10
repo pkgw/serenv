a814e24ce872761a6092eb726d9ffce36694e3e3
Peter Williams <peter@newton.cx>
2018-09-10 18:52:42 -0400

    src/main.rs: add cmd-format output

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@
 /// A tiny tool to save the current process environment to disk,
 /// and recover it later.
 
+// TODO all over here: avoid conversions to Cow<str>?
+
 extern crate bincode;
 extern crate failure;
 extern crate serde;
@@ -13,9 +15,10 @@ extern crate shell_escape;
 
 use bincode::{deserialize_from, serialize_into};
 use failure::{Error};
+use std::borrow::Cow;
 use std::collections::{HashMap, HashSet};
 use std::env;
-use std::ffi::OsString;
+use std::ffi::{OsStr, OsString};
 use std::fs::File;
 use structopt::StructOpt;
 
@@ -23,20 +26,25 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "serenv", about = "Save and restore the shell environment.")]
 enum SerenvCli {
-    #[structopt(name = "save")]
-    /// Save the current environment.
-    Save(SerenvSaveOptions),
+    #[structopt(name = "emit-cmd")]
+    /// Emit "cmd"-format commands to restore a saved environment.
+    EmitCmd(SerenvEmitCmdOptions),
 
     #[structopt(name = "emit-sh")]
     /// Emit "sh"-format commands to restore a saved environment.
     EmitSh(SerenvEmitShOptions),
+
+    #[structopt(name = "save")]
+    /// Save the current environment.
+    Save(SerenvSaveOptions),
 }
 
 impl SerenvCli {
     fn cli(self) -> Result<(), Error> {
         match self {
-            SerenvCli::Save(opts) => opts.cli(),
+            SerenvCli::EmitCmd(opts) => opts.cli(),
             SerenvCli::EmitSh(opts) => opts.cli(),
+            SerenvCli::Save(opts) => opts.cli(),
         }
     }
 }
@@ -48,15 +56,29 @@ fn main() -> Result<(), Error> {
 
 
 #[derive(Debug, StructOpt)]
-struct SerenvSaveOptions {
+struct SerenvEmitCmdOptions {
 }
 
-impl SerenvSaveOptions {
+impl SerenvEmitCmdOptions {
     fn cli(self) -> Result<(), Error> {
-        let env = SavedEnvironment::from_env();
-        let f = File::create(".serenv.dat")?;
-        serialize_into(f, &env)?;
+        let f = File::open(".serenv.dat")?;
+        let env: SavedEnvironment = deserialize_from(f)?;
+        let mut em = EmitCmdChanges {};
+        env.emit_changes(&mut em);
         Ok(())
+    }
+}
+
+struct EmitCmdChanges {}
+
+impl EmitChanges for EmitCmdChanges {
+    fn emit_unset(&mut self, key: &OsStr) {
+        println!("set {}=", shell_escape::windows::escape(key.to_string_lossy()));
+    }
+
+    fn emit_assign(&mut self, key: &OsStr, value: &OsStr) {
+        let s = format!("{}={}", key.to_string_lossy(), value.to_string_lossy());
+        println!("set {}", shell_escape::windows::escape(Cow::Borrowed(&s)));
     }
 }
 
@@ -69,7 +91,36 @@ impl SerenvEmitShOptions {
     fn cli(self) -> Result<(), Error> {
         let f = File::open(".serenv.dat")?;
         let env: SavedEnvironment = deserialize_from(f)?;
-        env.emit_sh();
+        let mut em = EmitShChanges {};
+        env.emit_changes(&mut em);
+        Ok(())
+    }
+}
+
+struct EmitShChanges {}
+
+impl EmitChanges for EmitShChanges {
+    fn emit_unset(&mut self, key: &OsStr) {
+        println!("unset {};", shell_escape::unix::escape(key.to_string_lossy()));
+    }
+
+    fn emit_assign(&mut self, key: &OsStr, value: &OsStr) {
+        println!("export {}={};",
+                 shell_escape::unix::escape(key.to_string_lossy()),
+                 shell_escape::unix::escape(value.to_string_lossy()));
+    }
+}
+
+
+#[derive(Debug, StructOpt)]
+struct SerenvSaveOptions {
+}
+
+impl SerenvSaveOptions {
+    fn cli(self) -> Result<(), Error> {
+        let env = SavedEnvironment::from_env();
+        let f = File::create(".serenv.dat")?;
+        serialize_into(f, &env)?;
         Ok(())
     }
 }
@@ -92,22 +143,20 @@ impl SavedEnvironment {
         SavedEnvironment { env }
     }
 
-    pub fn emit_sh(&self) {
-        // TODO all over here: avoid conversion to &str?
+    pub fn emit_changes<T: EmitChanges>(&self, emitter: &mut T) {
         let mut handled = HashSet::new();
 
         for (os_key, os_value) in env::vars_os() {
             match self.env.get(&os_key) {
                 None => {
-                    println!("unset {};", os_key.to_string_lossy());
+                    emitter.emit_unset(&os_key);
                 },
 
                 Some(saved_value) => {
                     if &os_value == saved_value {
                         // Nothing to do.
                     } else {
-                        println!("export {}={};", os_key.to_string_lossy(),
-                                 shell_escape::unix::escape(saved_value.to_string_lossy()));
+                        emitter.emit_assign(&os_key, saved_value);
                     }
 
                     handled.insert(os_key);
@@ -120,9 +169,15 @@ impl SavedEnvironment {
                 continue;
             }
 
-            println!("export {}={};", os_key.to_string_lossy(),
-                     shell_escape::unix::escape(os_value.to_string_lossy()));
+            emitter.emit_assign(os_key, os_value);
         }
     }
 }
 
+
+/// A helper trait for making emission pluggible
+
+trait EmitChanges {
+    fn emit_unset(&mut self, key: &OsStr);
+    fn emit_assign(&mut self, key: &OsStr, value: &OsStr);
+}
